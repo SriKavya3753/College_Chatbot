7b3129197c32267485491912c86e334e2244e56a
SriKavya3753 <123746746+SriKavya3753@users.noreply.github.com>
2025-02-28 14:53:53 +0530

    Update svecw_bot.py

--- svecw_bot.py
+++ svecw_bot.py
@@ -32,28 +32,28 @@ def find_closest_question(user_query, vectorizer, question_vectors, df):
     return df.iloc[best_match_index]['Answer']
   else:
     return None
- st.title("SVECW College Chatbot")
- st.write("Welcome to the College Chatbot! Ask me anything about the college.")
- for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
- if prompt := st.chat_input("Type your question here....."):
-  st.session_state.messages.append({"role":"user", "content": prompt})
-  with st.chat_message("user"):
-    st.markdown(prompt)
-  closest_answer = find_closest_question(prompt, vectorizer, question_vectors, df)  
-  if closest_answer:
-    st.session_state.messages.append({"role":"assistant", "content": closest_answer})
+st.title("SVECW College Chatbot")
+st.write("Welcome to the College Chatbot! Ask me anything about the college.")
+for message in st.session_state.messages:
+with st.chat_message(message["role"]):
+  st.markdown(message["content"])
+if prompt := st.chat_input("Type your question here....."):
+st.session_state.messages.append({"role":"user", "content": prompt})
+with st.chat_message("user"):
+  st.markdown(prompt)
+closest_answer = find_closest_question(prompt, vectorizer, question_vectors, df)  
+if closest_answer:
+  st.session_state.messages.append({"role":"assistant", "content": closest_answer})
+  with st.chat_message("assistant"):
+    st.markdown(closest_answer)
+else:
+  try:
+    response = model.generate_content(
+        f"You are a helpful and knowledgeable chatbot for SVECW College.Provide a detailed answer to the following question: {prompt}"
+    )
+    st.session_state.messages.append({"role":"assistant", "content": response.text})
     with st.chat_message("assistant"):
-      st.markdown(closest_answer)
-  else:
-    try:
-      response = model.generate_content(
-          f"You are a helpful and knowledgeable chatbot for SVECW College.Provide a detailed answer to the following question: {prompt}"
-      )
-      st.session_state.messages.append({"role":"assistant", "content": response.text})
-      with st.chat_message("assistant"):
-        st.markdown(response.text)
-    except Exception as e:
-      st.error(f"Sorry, I couldn't generate a response. Error: {e}")
+      st.markdown(response.text)
+  except Exception as e:
+    st.error(f"Sorry, I couldn't generate a response. Error: {e}")
       
